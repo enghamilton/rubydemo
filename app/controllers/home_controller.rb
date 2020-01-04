a00f873d8fbed581b9a0aa698b5abfbb89666d41
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2020-01-04 19:31:43 -0300

    Add files via upload

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+     
+    def product
+        @mensagem = "Hello world"
+        #@mensagem = “Hello world”
+        #@message = ‘Hello World!’
+        #@mensagem = {id:1, name:'user1 hamilton', phone:'912345678'}
+    end
+  
+end
